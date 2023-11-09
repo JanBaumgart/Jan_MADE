8d6fcfc4d077bcee2cfe4d341bc16760d663e78d
Jan Baumgart <92802641+JanBaumgart@users.noreply.github.com>
2023-11-09 10:41:00 +0100

    Inserted Datatyp decleration

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlalchemy
 from sqlalchemy import create_engine
 
 # URL in Variable speichern
@@ -10,11 +11,30 @@ db_path = 'airports.sqlite'
 # Die Daten von der URL abrufen und mit Pandas Funktion lesen. WICHTIG: Anmerken dass es mit Semikolon getrent wird! (https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html#pandas-read-csv)
 data = pd.read_csv(url, sep=';')
 
+# Datentypen der Spalten festlegen
+data_types = {
+    'column_1': sqlalchemy.types.BIGINT,
+    'column_2': sqlalchemy.types.TEXT,
+    'column_3': sqlalchemy.types.TEXT,
+    'column_4': sqlalchemy.types.TEXT,
+    'column_5': sqlalchemy.types.TEXT,
+    'column_6': sqlalchemy.types.TEXT,
+    'column_7': sqlalchemy.types.FLOAT,
+    'column_8': sqlalchemy.types.FLOAT,
+    'column_9': sqlalchemy.types.BIGINT,
+    'column_10': sqlalchemy.types.FLOAT,
+    'column_11': sqlalchemy.types.TEXT,
+    'column_12': sqlalchemy.types.TEXT,
+    'geo_punkt': sqlalchemy.types.TEXT
+}
+
+data['column_9'] = data['column_9'].astype(float)
+
 # Eine Verbindung mit einer lokalen SQLite-Datenbank herstellen. (https://docs.sqlalchemy.org/en/20/core/engines.html)
 engine = create_engine('sqlite:///' + db_path)
 
 # Die Daten in die Datenbank einfügen (https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html)
-data.to_sql('airports', engine, if_exists='replace', index=False)
+data.to_sql('airports', engine, if_exists='replace', index=False, dtype=data_types)
 
 # Datenbankverbindung beenden
 engine.dispose()
